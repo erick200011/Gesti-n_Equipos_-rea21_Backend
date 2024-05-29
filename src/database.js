f4a1a11e2be6f101ee052f8ae2475e41ce313f64
erick200011 <75103508+erick200011@users.noreply.github.com>
2024-05-29 13:39:26 -0500

    base de datos en render

--- src/database.js
+++ src/database.js
@@ -1,35 +1,27 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv'
 
+dotenv.config();
+
+
 // Configurar la cadena de conexión a tu base de datos PostgreSQL
-/*const connectionString = 'postgresql://postgres:IMQ2019@localhost:5432/Equipos1';
+const isProduction = process.env.NODE_ENV === 'production';
+const connectionString = isProduction ? process.env.DATABASE_URL_RENDER : process.env.DATABASE_URL_LOCAL;
+
+
 
 // Crear una nueva instancia de Sequelize
-const sequelize = new Sequelize(connectionString);
-
-export default sequelize;*/
-
-//=========================================//
-
-// Cargar variables de entorno desde un archivo .env (opcional)
-dotenv.config();
-// Configurar la cadena de conexión a tu base de datos PostgreSQL usando variables de entorno
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false // Esto puede ser necesario dependiendo de tu configuración de SSL
-        }
-      }
-    }
-  );
+const sequelize = new Sequelize(connectionString, {
+    dialect: 'postgres',
+    dialectOptions: isProduction ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Configura esto según tus necesidades
+      },
+    } : {}
+  });
   
   export default sequelize;
+
+//======================================================================================================//
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 // Importar el módulo del servidor
-//import app from './server.js';
-
-// funcion para render
-import express from 'express';
-
+import app from './server.js';
 // Importar la instancia de Sequelize desde database.js
 import sequelize from './database.js';
 
 // Configurar el puerto
-const app = express();
-
-//const port = app.get('port');
-
-const PORT = process.env.PORT || 3000;
+const port = app.get('port');
 
 // Escuchar en el puerto configurado
-/*app.listen(port, async () => {
+app.listen(port, async () => {
     console.log(`Server ok on http://localhost:${port}`);
     
     // Intentar conectar con la base de datos
@@ -25,19 +17,7 @@ const PORT = process.env.PORT || 3000;
     } catch (error) {
         console.error('Error al conectar con la base de datos:', error);
     }
-});*/
+});
 
-app.get('/', async (req, res) => {
-    try {
-      await sequelize.authenticate();
-      console.log('Conexión a la base de datos establecida correctamente.');
-      res.send('Conexión a la base de datos establecida correctamente.');
-    } catch (error) {
-      console.error('No se pudo conectar a la base de datos:', error);
-      res.status(500).send('Error en el servidor');
-    }
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-  });
+/*==========================================================================*/ 
+
