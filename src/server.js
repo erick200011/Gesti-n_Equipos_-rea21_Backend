b483b5bfd3a0991d64d8e16dbe543f3670ec7bf0
erick200011 <75103508+erick200011@users.noreply.github.com>
2024-07-05 09:40:15 -0500

    habilitar puertos 6

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
+// app.js
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import router from './routers/super_usuario_routes.js';
 import routerUsuariosArea from './routers/usuario_area_routes.js';
 import routerEquipos from './routers/Equipos_routes.js';
-import routerMantenimiento from './routers/Mantenimiento_routes.js';
-import routerVerificacion from './routers/Verificacion_routes.js';
-import routerCalibracion from './routers/Calibracion_routes.js';
+import routerMantenimiento from './routers/Mantenimiento_routes.js'
+import routerVerificacion from './routers/Verificacion_routes.js'
+import routerCalibracion from './routers/Calibracion_routes.js'
 
 // Inicializaciones
 const app = express();
@@ -14,14 +15,7 @@ dotenv.config();
 
 // Configuraciones
 app.set('port', process.env.PORT || 3000);
-
-// Configuración de CORS
-app.use(cors({
-    origin: 'https://gestion-de-equipos-frontend.netlify.app', // Asegúrate de no tener la barra diagonal al final
-    credentials: true, // Si necesitas enviar cookies
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
-    allowedHeaders: 'Content-Type,Authorization', // Encabezados permitidos
-}));
+app.use(cors());
 
 // Middlewares
 app.use(express.json());
@@ -30,12 +24,12 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Server on");
 });
-app.use('/api/superusuarios', router);
-app.use('/api/usuariosarea', routerUsuariosArea);
-app.use('/api/equipos', routerEquipos);
-app.use('/api/mantenimiento', routerMantenimiento);
-app.use('/api/verificacion', routerVerificacion);
-app.use('/api/calibracion', routerCalibracion);
+app.use('/api', router);
+app.use('/api', routerUsuariosArea);
+app.use('/api', routerEquipos);
+app.use('/api', routerMantenimiento);
+app.use('/api', routerVerificacion);
+app.use('/api', routerCalibracion);
 
 // Manejo de ruta no encontrada
 app.use((req, res) => res.status(404).send("Endpoint no encontrado - 404"));
